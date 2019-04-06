48a843ae5149c8b3c289105fcffa2ed6f6c303be
Uncle Charlie <lixiangdong@mobike.com>
2019-04-06 19:41:17 +0800

    auth to auth controller

--- controllers/RunnerController.go
+++ controllers/RunnerController.go
@@ -1,16 +1,10 @@
 package controllers
 
-import (
-	"net/http"
+import "github.com/labstack/echo/v4"
 
-	"github.com/labstack/echo/v4"
-)
-
-// RunnerController runs something
 type RunnerController struct {
 }
 
-// RunnerHandler handles run directive
-func (runner *RunnerController) RunnerHandler(c echo.Context) error {
-	return c.String(http.StatusOK, "hello, this is runner")
+func (runner *RunnerController) RunnerHandler(e echo.Context) error {
+
 }
