b2f9ecc7c68d2c481f112c53e34261e558f83867
Uncle Charlie <lixiangdong@mobike.com>
2019-04-09 00:29:51 +0800

    prepare parse uploaded scripts

---
+++ services/parser_test.go
@@ -0,0 +1,40 @@
+package services
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"testing"
+)
+
+func TestParser(t *testing.T) {
+	targetFile, err := filepath.Abs("../store/tmp/file_list.txt")
+	if err != nil {
+		t.Error("Can not get target file")
+	}
+
+	dir, err := ioutil.ReadDir("./")
+	if err != nil {
+		t.Error("Read dir failed")
+	}
+
+	var f *os.File
+	// if _, err := os.Stat(targetFile); os.IsNotExist(err) {
+	// Create a new file
+	f, err = os.Create(targetFile)
+	if err != nil {
+		t.Errorf("Create target file error %v", err)
+	}
+	// }
+
+	for _, file := range dir {
+		fmt.Printf("File name:- %s\n", file.Name())
+		// err = ioutil.WriteFile(targetFile, []byte(file.Name()), 0644)
+		_, err = f.WriteString(fmt.Sprintf("%s\n", file.Name()))
+		if err != nil {
+			t.Errorf("Write file failed: %v", err)
+			return
+		}
+	}
+}
