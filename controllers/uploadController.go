2d063967c4c2bd7b4aa594328baebe6e649354e2
Uncle Charlie <lixiangdong@mobike.com>
2019-04-15 23:54:01 +0800

    adjust for the cli uploader

--- controllers/uploadController.go
+++ controllers/uploadController.go
@@ -16,24 +16,35 @@ import (
 type UploadController struct {
 }
 
-// UploadHandler will handle user's upload
+// UploadHandler will handle user's upload.
+// Accept mutiple files upload
 // 1. Store uploaded scripts
-// 2. Process these scripts
+// 2. Process these scripts, when the complete uploading api is called
 // 3. Run docker
 // TODO:
 // 1. Process files need to add require in these scripts according to some conditionn
 func (uploader *UploadController) UploadHandler(c echo.Context) error {
-	fmt.Println("Hello Uploader")
+	fmt.Printf("Hello Uploader %v\n", c.Request())
+	fileType := c.FormValue("type")
 	file, err := c.FormFile("file")
 	if err != nil {
 		return err
 	}
 
-	// 1. Store files
+	fmt.Printf("==>Upload type: %s, file name: %s\n", fileType, file.Filename)
+
+	// Store files
 	if err := uploader.storeFile(file); err != nil {
 		return err
 	}
 
+	return c.HTML(http.StatusOK,
+		fmt.Sprintf("<p>File %s uploaded successfully with fields.</p>",
+			file.Filename))
+}
+
+// UploadCompleteHandler indicates that upload of all files are done
+func (uploader *UploadController) UploadCompleteHandler(c echo.Context) error {
 	// Process files
 	parser, err := services.NewParser("", "")
 	if err != nil {
@@ -47,11 +58,10 @@ func (uploader *UploadController) UploadHandler(c echo.Context) error {
 	// Execute runner
 	services.Run()
 
-	return c.HTML(http.StatusOK,
-		fmt.Sprintf("<p>File %s uploaded successfully with fields.</p>",
-			file.Filename))
+	return c.String(http.StatusOK, "Upload done")
 }
 
+// TODO: move this function to services
 func (uploader *UploadController) storeFile(file *multipart.FileHeader) error {
 	src, err := file.Open()
 	if err != nil {
@@ -59,7 +69,7 @@ func (uploader *UploadController) storeFile(file *multipart.FileHeader) error {
 	}
 	defer src.Close()
 
-	storePath, err := filepath.Abs(fmt.Sprintf("./store/temp/%s", file.Filename))
+	storePath, err := filepath.Abs(fmt.Sprintf("./store/tmp/%s", file.Filename))
 	if err != nil {
 		return fmt.Errorf("Get store path error %v", err)
 	}
